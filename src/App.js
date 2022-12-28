145eba5114f59b71b003202fd1dde9c57bb2de1a
Rayene Amaro <96782425+RayeneAmaro@users.noreply.github.com>
2022-12-28 15:02:36 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./pages/Login";
+import GlobalStyle from "./global";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
